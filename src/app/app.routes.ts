1fc20baf4eccf5ef1a558a77aaa7c99b8cede360
porostIslandzki <318632@office.umcs.pl>
2025-05-05 12:30:38 +0200

    changes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RoomsearchComponent } from './roomsearch/roomsearch.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GuardService } from './service/guard.service';
+import { EditprofileComponent } from './editprofile/editprofile.component';
 
 export const routes: Routes = [
   { path: '', component: RoomsearchComponent }, // <-- główna strona
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  // dodaj kolejne trasy według potrzeb
+  { path: 'profile', component: ProfileComponent, canActivate: [GuardService]},
+  { path: 'edit-profile', component: EditprofileComponent, canActivate: [GuardService]},
+  {path: '**', redirectTo: 'home'}
 ];
